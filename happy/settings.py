4b80be8606ccc8d32d6cc2e111f6003acc5c8481
unknown <pitermichkail@ymail.com>
2022-07-25 13:23:40 +0700

    AllowedHost

--- happy/settings.py
+++ happy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('ADS_GACK'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'rice-paper.herokuapp.com']
 
 
 # Application definition
